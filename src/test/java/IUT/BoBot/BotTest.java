246ea0d22ff2391e663503013b0b8586d6f71ce3
Catherine Farrahi Far <catherine.farrahifar@gmail.com>
2014-02-26 09:55:14 +0100

    Class Salut et toute les classes parallèle fait

--- src/test/java/IUT/BoBot/BotTest.java
+++ src/test/java/IUT/BoBot/BotTest.java
@@ -32,4 +32,10 @@ public class BotTest
     {
         assertEquals(null, bot.ask("This is not a question."));
     }
+    
+    @Test
+    public void testSalut()
+    {
+    	assertEquals("Salut!", bot.ask("Salut"));
+    }
 }
